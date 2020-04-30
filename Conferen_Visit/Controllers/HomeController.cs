5a63ee32c581083d95a5ff57f8bb5ba0adfb757a
yuliia-h <yuliiagusak@gmail.com>
2020-04-30 17:09:46 +0300

    Add project files.

---
+++ Conferen_Visit/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Conferen_Visit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Conferen_Visit.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morging" : "Good afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+                return View("Thanks", guest);
+
+            else
+                return View();
+        }
+    }
+}
---
+++ Conferen_Visit/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Conferen_Visit.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please, enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "You entered an incorrect password")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please, enter your phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please, indicate whether you will attend the party")]
+        public bool? WillAttend { get; set; }
+    }
+}
